73335e4524c6281c1783add9e522fd1b7e6f96a9
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-03 14:37:07 -0500

    functionality complete needs styling

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ class App extends Component {
     score: 0,
     highScore: 0,
     clicked: clicked,
-    images: images
+    images: images,
+    status: "Ok trainer choose a Pokemon!"
   }
 
   //i originally tried to set state by passing in previous state and pushing but that didn't seem to work - i wanted to avoid using global variables but thats the only way i could make it work the way i wanted - i'll ask ta about this but if you are grading and know what i mean would appreciate some insight
@@ -46,18 +47,26 @@ class App extends Component {
       alert("You already clicked that!")
       clicked = [];
       shuffle(images);
-      this.setState({ clicked: clicked, images: images });
+        if (this.state.score > this.state.highScore) {
+          this.setState({ clicked: clicked, images: images, score: 0, highScore: this.state.score, status: "Good work trainer you earned a high score!" })
+        } else {
+          this.setState({ clicked: clicked, images: images, score: 0, status: "You need more badges trainer!" })
+        };
     } else if (!clicked.includes(e.target.id)) {
       alert ("You clicked " + e.target.id)
       clicked.push(e.target.id);
       shuffle(images);
-      this.setState({ clicked: clicked, images: images });
+      this.setState({ clicked: clicked, images: images, score: this.state.score + 1, status: "It's super effective!" });
     }
   }
   render() {
     return (
       <div>
-        <StatusBoard />
+        <StatusBoard
+          score={this.state.score}
+          highScore={this.state.highScore}
+          status={this.state.status}
+        />
         <Grid
           images={this.state.images}
           handleClick={this.handleClick}
--- src/components/StatusBoard.js
+++ src/components/StatusBoard.js
@@ -4,10 +4,10 @@ const StatusBoard = props => (
     <div className="board">
         <h1 className="title"> Remember The Mon!</h1>
         <h3>Instructions: Click a Pokemona and you get a point, but don't click the same Pokemon more than once!</h3>
-        <h3>Current status: It's super effective!</h3>
+        <h3>Current status: {props.status}</h3>
         <div className="scores">
-            <p>Score: 0</p>
-            <p>High Scre: 0</p>
+            <p>Score: {props.score}</p>
+            <p>High Score: {props.highScore}</p>
         </div>
     </div>
 )
