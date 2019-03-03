6d59fa62d6a37e686a25442a449b448437251e9d
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-02 20:53:21 -0500

    heroku commit

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ class App extends Component {
     images: ["alakazam", "arcanine", "charizard", "clefairy", "eevee", "gengar", "jigglypuff", "mewtwo", "ninetales", "pikachu", "poliwrath", "psyduck", "scyther", "snorlax", "tentacruel"]
   }
 
-  handleClick(e){
-    alert("You clicked " + e.target.id)
-  }
-
-  // handleClick(e) {
-  //   if (this.state.clicked !== e) {
-  //     alert("You clicked " + e.target.id)
-  //   } else if (this.state.clicked === e) {
-  //     alert("You already clicked that!")
-  //   }
-
+  // handleClick(e){
+  //   alert("You clicked " + e.target.id)
   // }
+
+  handleClick(e) {
+    if (this.state.clicked !== e) {
+      alert("You clicked " + e.target.id)
+    } else if (this.state.clicked === e) {
+      alert("You already clicked that!")
+    }
+
+  }
   render() {
     return (
     <Grid 
